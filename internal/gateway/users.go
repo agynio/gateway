2edfadfb560ba0e65123bd1007672ba2347b51b4
Casey Brooks <casey.brooks@agyn.io>
2026-03-25 12:52:21 +0000

    feat(gateway): add batch users handler

--- internal/gateway/users.go
+++ internal/gateway/users.go
@@ -15,6 +15,17 @@ func NewUsersGateway(users usersv1.UsersServiceClient) *UsersGateway {
 	return &UsersGateway{users: users}
 }
 
+func (g *UsersGateway) BatchGetUsers(ctx context.Context, req *connect.Request[usersv1.BatchGetUsersRequest]) (*connect.Response[usersv1.BatchGetUsersResponse], error) {
+	resp, err := g.users.BatchGetUsers(ctx, req.Msg)
+	if err != nil {
+		return nil, toConnectError(err)
+	}
+	for _, u := range resp.Users {
+		u.OidcSubject = ""
+	}
+	return connect.NewResponse(resp), nil
+}
+
 func (g *UsersGateway) CreateAPIToken(ctx context.Context, req *connect.Request[usersv1.CreateAPITokenRequest]) (*connect.Response[usersv1.CreateAPITokenResponse], error) {
 	resp, err := g.users.CreateAPIToken(ctx, req.Msg)
 	if err != nil {
