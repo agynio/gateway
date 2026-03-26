41b1dd23484d684547f761afd5b47e577094314b
Casey Brooks <casey.brooks@agyn.io>
2026-03-26 01:09:31 +0000

    feat(gateway): add EnrollApp proxy

--- internal/gateway/apps.go
+++ internal/gateway/apps.go
@@ -15,6 +15,14 @@ func (g *Gateway) RegisterApp(ctx context.Context, req *connect.Request[appsv1.R
 	return connect.NewResponse(resp), nil
 }
 
+func (g *Gateway) EnrollApp(ctx context.Context, req *connect.Request[appsv1.EnrollAppRequest]) (*connect.Response[appsv1.EnrollAppResponse], error) {
+	resp, err := g.apps.EnrollApp(ctx, req.Msg)
+	if err != nil {
+		return nil, toConnectError(err)
+	}
+	return connect.NewResponse(resp), nil
+}
+
 func (g *Gateway) GetApp(ctx context.Context, req *connect.Request[appsv1.GetAppRequest]) (*connect.Response[appsv1.GetAppResponse], error) {
 	resp, err := g.apps.GetApp(ctx, req.Msg)
 	if err != nil {
