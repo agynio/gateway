8b6aff7d65ab321125ab270fbb13d333995b55da
Casey Brooks <casey.brooks@agyn.io>
2026-04-18 21:47:45 +0000

    feat(gateway): forward org threads

--- internal/gateway/threads.go
+++ internal/gateway/threads.go
@@ -56,6 +56,22 @@ func (g *ThreadsGateway) GetThreads(ctx context.Context, req *connect.Request[th
 	return connect.NewResponse(resp), nil
 }
 
+func (g *ThreadsGateway) ListOrganizationThreads(ctx context.Context, req *connect.Request[threadsv1.ListOrganizationThreadsRequest]) (*connect.Response[threadsv1.ListOrganizationThreadsResponse], error) {
+	resp, err := g.threads.ListOrganizationThreads(ctx, req.Msg)
+	if err != nil {
+		return nil, toConnectError(err)
+	}
+	return connect.NewResponse(resp), nil
+}
+
+func (g *ThreadsGateway) GetThread(ctx context.Context, req *connect.Request[threadsv1.GetThreadRequest]) (*connect.Response[threadsv1.GetThreadResponse], error) {
+	resp, err := g.threads.GetThread(ctx, req.Msg)
+	if err != nil {
+		return nil, toConnectError(err)
+	}
+	return connect.NewResponse(resp), nil
+}
+
 func (g *ThreadsGateway) GetMessages(ctx context.Context, req *connect.Request[threadsv1.GetMessagesRequest]) (*connect.Response[threadsv1.GetMessagesResponse], error) {
 	resp, err := g.threads.GetMessages(ctx, req.Msg)
 	if err != nil {
